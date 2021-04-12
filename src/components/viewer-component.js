94e0cf8b62c361d74be8bdaab66e1d2f6ac11734
Jingsong Tan <jingsong.tan@techsoft3d.com>
2021-04-12 00:13:09 -0500

    microengine displayed

--- src/components/viewer-component.js
+++ src/components/viewer-component.js
@@ -1,19 +1,33 @@
 import { Component } from "react";
 import { v4 as uuidv4 } from "uuid";
+import microengine from '../assets/microengine.scs';
+import Communicator from "communicator";
 // import Communicator from "https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@2020.0.0/hoops_web_viewer.js?v=2021.0.0";
 
 class ViewerComponent extends Component {
     constructor(props) {
         super(props);
-        // this.hwp = new Communicator.WebViewer({
-        //     containerId: this.viewerId,
-        //     endpointUri: this.modelPath,
-        //   });
+    }
+
+    componentDidMount() {
+        console.log(microengine);
+        var hwv = new Communicator.WebViewer({
+            containerId: "viewerId",
+            endpointUri: microengine,
+        });
+
+        hwv.setCallbacks({
+            sceneReady: () => {
+                hwv.view.setBackgroundColor(Communicator.Color.blue(), Communicator.Color.blue());
+            },
+        });
+
+        hwv.start();
     }
 
     render() {
         return (
-            <div>Web Viewer {uuidv4()}</div>
+            <div className="bg-light position-relative" id="viewerId" style={{width: "500px", height: "500px"}}></div>
         );
     }
 }
